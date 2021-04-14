54f46b1bbb5d55a2adc54268c7ec5203c222215c
Vasile-Hij <vasile_hijs@yahoo.com>
2021-04-14 22:54:28 +0300

    Day 1 update and Day 2

--- day_1.py
+++ day_1.py
@@ -3,35 +3,40 @@
 https://adventofcode.com/2020/day/1
 """
 
-with open('./files/day_1.txt', 'r') as text_input:
-    expenses = text_input.readlines()
+def main():
+    with open('./files/day_1.txt', 'r') as text_input:
+        expenses = text_input.readlines()
 
-nums = [int(line.strip()) for line in expenses]
-
-
-def day1_1(num):
-    """Find the distinct numbers that sum to 2020, and return their product"""
-    for num_1 in num:
-        num_2 = 2020 - num_1
-        if num_2 in num:
-            answer = num_1 * num_2
-            print(answer)
-            break
+    nums = [int(line.strip()) for line in expenses]
 
 
-def day1_2(num):
-    """Find the distinct numbers that sum to 2020, and return their product"""
-    for num_1 in num:
-        for num_2 in num:
-            for num_3 in num:
-                if num_1 + num_2 + num_3 == 2020:
-                    answer = num_1 * num_2 * num_3
-                    print(answer)
-                    break
+    def day1_1(num):
+        """Find the distinct numbers that sum to 2020, and return their product"""
+        for num_1 in num:
+            num_2 = 2020 - num_1
+            if num_2 in num:
+                answer = num_1 * num_2
+                print(answer)
+                break
 
 
-day1_1(nums)
-day1_2(nums)
+    def day1_2(num):
+        """Find the distinct numbers that sum to 2020, and return their product"""
+        for num_1 in num:
+            for num_2 in num:
+                for num_3 in num:
+                    if num_1 + num_2 + num_3 == 2020:
+                        answer = num_1 * num_2 * num_3
+                        print(answer)
+                        break
+
+
+    day1_1(nums)
+    day1_2(nums)
+
+if __name__ == '__main__':
+    main()
+
 """
 Part one: Your puzzle answer was 969024.
 Part two: Your puzzle answer was 230057040.
---
+++ day_2.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+https://adventofcode.com/2020/day/2
+
+Part 1
+Each line gives the password policy and then the password. The password policy indicates
+the lowest and highest number of times a given letter must appear for the password to be valid.
+For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
+How many passwords are valid according to their policies?
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+
+
+Part 2
+Each policy actually describes two positions in the password,
+where 1 means the first character, 2 means the second character, and so on.
+(Be careful; Toboggan Corporate Policies have no concept of "index zero"!)
+Exactly one of these positions must contain the given letter.
+Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+"""
+
+with open('./files/day_2.txt', 'r') as f:
+    passwords = f.read().splitlines()
+
+total = len(passwords)
+
+
+def day_2_part_1():
+    valid = 0
+
+    for x in passwords:
+        numbers, letter, valid_pass = x.split(" ")
+        lower, higher = numbers.split("-")
+        letters = letter.split(":")[0]
+        counter = valid_pass.count(letters)
+        # print(lower, higher, letters, valid_pass, counter)
+        if int(lower) <= counter <= int(higher):
+            valid += 1
+
+    print(f'Valid passwords: %d ' % valid)
+    print(f'Total passwords: %d' % total)
+
+
+def day_2_part_2():
+    valid = 0
+
+    for x in passwords:
+        number, letter, valid_pass = x.split(' ')
+        lower, higher = map(int, number.split('-'))
+        password_1 = valid_pass[lower-1] == letter[0]
+        password_2 = valid_pass[higher-1] == letter[0]
+
+        if password_1 != password_2:
+            valid += 1
+
+    print(f'Valid passwords: %d' % valid)
+    print(f'Total passwords: %d' % total)
+
+
+def passwords_policy():
+    print(f'Password validated with passwords policy!')
+    day_2_part_1()
+    print()
+    print(f'Updated password policy according Toboggan Corporate Policies.')
+    day_2_part_2()
+
+
+if __name__ == '__main__':
+    passwords_policy()
+
+"""
+Part 1
+Output:
+Valid passwords: 524 
+Total passwords: 1000
+
+Part 2
+Valid passwords: 485
+"""
