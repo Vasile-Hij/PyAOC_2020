5fa04dd96df83507837a0ae891960bd71f728afd
Vasile-Hij <vasile_hijs@yahoo.com>
2021-05-27 00:10:25 +0300

    day5 part2

--- day_5.py
+++ day_5.py
@@ -2,6 +2,7 @@
 """
 https://adventofcode.com/2020/day/5
 
+Part I:
 This challenge consists in finding seat in airplane where airport terminal are using
 binary space partitioning, so FBFBBFFRLR, where F means "front", B means "back",
 L means "left", and R means "right".
@@ -24,6 +25,14 @@ The final R keeps the upper of the two, column 5.
 
 So, decoding FBFBBFFRLR reveals that it is the seat at row 44, column 5, multiply by 8.
 ID: 44 * 8 + 5 = 357
+
+
+Part II:
+Some of the seats at the very front and back of the plane don't exist on this aircraft,
+so they'll be missing from your list as well.
+Your seat wasn't at the very front or back, though; the seats with IDs +1 and -1
+from yours will be in your list.
+What is the ID of your seat?
 """
 
 # Approaching method: using binary system F=0, B=1, R=1, L=0
@@ -41,8 +50,24 @@ with open('./files/day_5.txt', 'r') as f:
 
 
 def main():
-    #For each seat, convert it to binary, and grab the max in the array.
-    print("Part 1: %s" % {max([int(s, 2) for s in flight_ticket_seat])})
+    # For each seat, convert it to binary, and grab the max in the array.
+    """Part 1 for first compilation"""
+    # print("Part 1: %s" % {max([int(s, 2) for s in flight_ticket_seat])})
+
+    """Part 1"""
+    occupied_seats = set([int(s, 2) for s in flight_ticket_seat])
+    print("Part 1: %s" % (max(occupied_seats)))
+
+    """Part 2 - it goes through all seats and the ID ticket"""
+    allseats = set(range(128 * 8))
+    openseats = allseats - occupied_seats
+    myseat = [
+        seat
+        for seat in openseats
+        if seat + 1 not in openseats and seat - 1 not in openseats
+    ]
+
+    print(f"Part 2: {myseat[0]}")
 
 
 if __name__ == '__main__':
@@ -51,5 +76,5 @@ if __name__ == '__main__':
 """
 *Results*
 Part 1: 866
-Part 2: 
+Part 2: 583
 """
